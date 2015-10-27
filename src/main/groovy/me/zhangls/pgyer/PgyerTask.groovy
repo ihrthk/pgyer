a1b1afa9e62395de3629838fe1f6799f1347690e
zhangls <zhanglishun@letvpicture.com>
2015-10-27 17:50:20 +0800

    add task desc

--- src/main/groovy/me/zhangls/pgyer/PgyerTask.groovy
+++ src/main/groovy/me/zhangls/pgyer/PgyerTask.groovy
@@ -9,6 +9,26 @@ import java.util.concurrent.TimeUnit
 
 class PgyerTask extends DefaultTask {
 
+    {
+        group = 'pgyer'
+        description = """Gradle plugin for pgyer.com upload
+        See https://github.com/ihrthk/pgyer for more information.
+        Available configuration options and default values:
+        //用户Key 点击获取uKey
+        String uKey
+        //API Key 点击获取API Key
+        String _api_key
+        //需要上传的ipa或者apk文件
+        File file
+        //(ipa上传时为必填) 填写发布范围，值为（2，3），2：直接发布，3：只有我安装
+        String publishRange
+        //(选填) 是否发布到广场，值为（1，2），1：发布到广场，2：不发布到广场。默认为不发布到广场
+        String isPublishToPublic
+        //(选填) 设置App安装密码，如果不想设置密码，请传空字符串，或不传。
+        String password
+        //(选填) 版本更新描述，请传空字符串，或不传。
+        String updateDescription """
+    }
     private final String API_END_POINT = "http://www.pgyer.com/apiv1"
 
     @TaskAction
